ba60fe3046afc01bd4c662a3f95ad00da234eab2
amrutha104 <amruthavarshini.b@iamneo.ai>
2024-07-01 17:40:25 +0530

    Update config.js

--- reactapp/src/components/config.js
+++ reactapp/src/components/config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = 'https://8080-edbbcdbcdfbeebb313889347baedeafbdefebnine.premiumproject.examly.io';
+const API_BASE_URL = 'https://ffo2zjdxj3.execute-api.us-east-1.amazonaws.com/backend';
 
 export default API_BASE_URL;
