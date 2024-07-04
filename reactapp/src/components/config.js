d60b88cf998e437785068bd283c51d17cb922f38
amrutha104 <amruthavarshini.b@iamneo.ai>
2024-07-04 14:04:33 +0530

    Update config.js

--- reactapp/src/components/config.js
+++ reactapp/src/components/config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = 'https://ffo2zjdxj3.execute-api.us-east-1.amazonaws.com/backend';
+const API_BASE_URL = 'https://ytskn1hvff.execute-api.us-east-1.amazonaws.com/backend';
 
 export default API_BASE_URL;
